109b42871f65cbb117c297ed86794be02dc6bb28
Arunsing Rajput <arunsinghrajput2005@gmail.com>
2026-07-21 20:50:29 +0530

    LeetCode : Best Time to Buy and Sell Stock

---
+++ src/leetcode/Best_time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Best_time_to_Buy_and_Sell_Stock {
+    public int maxProfit(int[] prices) {
+
+        return 0;
+    }
+
+    public static void main(String[] args){
+        int[] prices = {7,1,5,3,6,4};
+
+        Best_time_to_Buy_and_Sell_Stock bs = new Best_time_to_Buy_and_Sell_Stock();
+        int[] ans = new int[]{bs.maxProfit(prices)};
+        System.out.println(ans);
+
+    }
+
+}
